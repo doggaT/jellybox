48f3490b3d43358ac9ad12be7bed60022521eb95
Douglas Tiepolo <dvtiepolo@gmail.com>
2023-12-09 16:29:07 +0000

    index test

--- test/controllers/about_controller_controller_test.rb
+++ test/controllers/about_controller_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class AboutControllerControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
-    get :about
+    get "/about"
     assert_response :success
   end
 end
