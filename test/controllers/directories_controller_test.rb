cc97559f99fdf1d08a8d3fcfe00a6af5bb42b222
Douglas Tiepolo <dvtiepolo@gmail.com>
2023-12-12 21:58:20 +0000

    fix test

--- test/controllers/directories_controller_test.rb
+++ test/controllers/directories_controller_test.rb
@@ -1,68 +1,67 @@
 require "test_helper"
 
 class DirectoriesControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::ControllerHelpers
-
   test "unauthenticated user should not get directories page" do
     get directories_path
     assert_response :redirect
     assert_redirected_to new_user_session_path
   end
 
-  # setup do
-  #   get '/users/sign_in'
-  #   sign_in users(:user_01)
-  #   post user_session_url
-  #
-  #   follow_redirect!
-  #   assert_response :success
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = User.create(username: 'test one',
+                        email: 'test_one@email.com',
+                        encrypted_password: "<%= Devise::Encryptor.digest(User, 'password') %>")
+
+
+  end
 
   test "authenticated users can navigate to directories" do
-    user = users(:one)
-    sign_in user
+    sign_in @user
+
     get directories_path
     assert_response :success
   end
 
   test "authenticated users can create directory" do
-    user = users(:one)
-    sign_in user
+    sign_in @user
+
     get new_directory_path
     assert_response :success
 
-    Directory.new(name: 'images')
+    Directory.create(name: 'images')
+
     get directories_path
-    assert_text 'Images'
+    assert_select 'td', text: 'Images'
   end
 
   test "authenticated users can create subdirectory" do
-    user = users(:one)
-    sign_in user
+    sign_in @user
+
     get new_directory_path
     assert_response :success
 
-    images_directory = Directory.new(name: 'images')
-    Directory.new(name: 'sub', parent_id: images_directory.id)
+    images_directory = Directory.create(name: 'images')
+
+    Directory.create(name: 'sub', parent_id: images_directory.id)
     get "/directories/#{images_directory.id}"
-    assert_text 'Sub'
+    assert_select 'td', text: 'Sub'
   end
 
   test "authenticated users can upload a file to a directory" do
-    user = users(:one)
-    sign_in user
+    sign_in @user
+
     get new_directory_path
     assert_response :success
 
-    Directory.new(name: 'images')
+    images_directory = Directory.create(name: 'images')
 
-    images_directory = Directory.where(name: 'images')
-
-    file_path = '../fixtures/files/test_file1.jfif'
+    file_path = 'test/fixtures/files/test_file1.jfif'
     filename = File.basename(file_path)
     images_directory.files.attach(io: File.open(file_path), filename: filename)
 
     get "directories/#{images_directory.id}"
-    assert_text 'test_file1.jfif'
+    assert_select 'td', text: 'test_file1.jfif'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,16 @@ require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors, with: :threads)
-  fixtures :all
+  # parallelize(workers: :number_of_processors, with: :threads)
+  fixtures :all, :users
 
   include Devise::Test::IntegrationHelpers
-  include Warden::Test::Helpers
+
+  def log_in(user)
+    sign_in(user)
+  end
+end
+
+class ActionController::TestCase
+  include Devise::Test::ControllerHelpers
 end
